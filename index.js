4760c3170f62d8f6195d694ed5376cc977aef98e
sanha <kgpaper@naver.com>
2021-03-08 04:04:53 +0900

    21/03/07 study

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ const title = document.querySelector("#title");
 
 const CLICKED_CLASS = "clicked";
 
-function handleClick(){
-    title.classList.toggle(CLICKED_CLASS);
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
 }
 
-function init(){
-    title.addEventListener("click", handleClick);
+function init() {
+  title.addEventListener("click", handleClick);
 }
 
 init();
-
-
-
 
 // 이벤트 관련 함수는 MDN참조
