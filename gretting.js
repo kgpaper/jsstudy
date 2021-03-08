e8740a4a04cca78b4ec00cbe343f8c8e445ad5f7
sanha <kgpaper@naver.com>
2021-03-09 00:58:04 +0900

    21/03/08 study

--- gretting.js
+++ gretting.js
@@ -1,28 +1,43 @@
-const form = document.querySelector(".js-form"), 
+const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
-function paintGreeting(text){
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText= `Hello ${text}`
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
 }
 
-function paintGreeting(text){
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null){
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
 
-  } else{
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
     paintGreeting(currentUser);
   }
 }
 
-
-function init(){
-
+function init() {
+  loadName();
 }
 
 init();
